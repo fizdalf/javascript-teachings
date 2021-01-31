6eecb909545cfb0609f3572df2c307dc4fc1dfec
fizdalf <fizdalf@gmail.com>
2021-01-31 10:13:37 +0000

    Primer Comit

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+// Escribe un programa que pida una palabra y escriba cuántas de las letras que tiene son vocales
+
+// let palabra = nuestroArray[2];
+//
+// console.log(palabra);
+// console.log(palabra[0])
---
+++ index.js
@@ -0,0 +1,38 @@
+let nuestroArray = process.argv;
+
+// Crear un programa que recibe 3 parámetros, comienzo, fin y paso, y muestra por pantalla el conteo desde comienzo hasta fin contando de "paso" en "paso".
+
+// let comienzo = parseInt(nuestroArray[2]);
+// let fin = parseInt(nuestroArray[3])
+// let paso = parseInt(nuestroArray[4])
+
+//comienzo++ // comienzo = comienzo + 1
+
+// while (comienzo < fin) {
+//     comienzo = comienzo + paso;
+//     console.log(comienzo)
+// }
+
+//
+// do {
+//     console.log(comienzo);
+//     comienzo = comienzo + paso;
+// } while (comienzo < fin)
+//
+//
+// for (let i = 0; i < nuestroArray.length; i++) {
+//     console.log(nuestroArray[i]);
+// }
+//
+//
+// let i = 0;
+// while (i < nuestroArray.length) {
+//     console.log(nuestroArray[i]);
+//     i++;
+// }
+
+
+let palabra = nuestroArray[2];
+
+console.log(palabra);
+console.log(palabra[0])
