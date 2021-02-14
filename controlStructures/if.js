7f3a5360efa3b3832ef17e8c908f17cc9b36e7ed
fizdalf <fizdalf@gmail.com>
2021-02-14 12:35:24 +0000

    Primer Comit

---
+++ controlStructures/if.js
@@ -0,0 +1,18 @@
+const arguments = process.argv;
+let rainChance = parseInt(arguments[2]);
+console.log(rainChance);
+const minimumRainChanceToBringUmbrella = 45;
+
+ ===
+ !==
+ >
+ <
+ >=  4 >= 4 -> true 4 >= 3 -> true 4 >= 7 -> false
+ <=  4 <= 4 -> true 4 <= 3 -> false 4 <= 7 -> true
+
+ 4 > 4 4 || 4 === 4
+
+  true || true -> true
+  true || false -> true
+  false || true -> true
+  false || false -> false
