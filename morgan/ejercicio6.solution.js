195339137e53c70252aac047d3089ff66d454a78
fizdalf <fizdalf@gmail.com>
2021-02-16 12:54:50 +0000

    Morgan solution to exercise 6

---
+++ morgan/ejercicio6.solution.js
@@ -0,0 +1,23 @@
+'use strict';
+// dada una frase por el terminal, determinar si la frase es o no un palíndromo, ten en cuenta que los espacios NO SE TIENEN EN CUENTA!!
+const checkIsPalindromeModule = require("../controlStructures/functions");
+const terminalArguments = process.argv;
+let index = 2;
+let phrase = "";
+const lastIndex = terminalArguments.length - 1
+while (index <= lastIndex) {
+    phrase = phrase + terminalArguments[index];
+    index++;
+}
+phrase = phrase.toLowerCase();
+
+let wordToCheck = phrase;
+const isPalindrome = checkIsPalindromeModule.checkIsPalindrome(wordToCheck);
+
+if (isPalindrome) {
+    console.log("Tu frase es un palíndromo");
+} else {
+    console.log('Tu frase no es un palíndromo');
+}
+
+
