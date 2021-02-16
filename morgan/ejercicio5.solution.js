c940101ec77019d13639a08531fbda6742c41732
fizdalf <fizdalf@gmail.com>
2021-02-16 11:37:09 +0000

    Morgan solution to exercise 5

--- morgan/ejercicio5.solution.js
+++ morgan/ejercicio5.solution.js
@@ -1,9 +1,12 @@
 // calcula la letra de un DNI sabiendo que tenemos que hacer una divisón entera entre el número dado y 23, una vez que tengamos el resto
 // debemos obtener la letra que tenga como índice el valor obtenido de la lista de abajo.
 const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+const arguments = process.argv;
+let IDNumber = parseInt(arguments[2]);
+const remainder = IDNumber % 23;
+let calculatedLetter = letras[remainder]
+console.log(calculatedLetter)
 
-const test = 2;
 
-if (true) {
-    console.log('Nice');
-}
+
+
