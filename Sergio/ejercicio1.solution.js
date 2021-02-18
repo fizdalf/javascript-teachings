4c5f54c80d117e60a6dfa05b6defd34c5c53d731
sergi <sergioarosafdez@gmail.com>
2021-02-18 20:46:48 +0000

    ejercicio1 completado

--- Sergio/ejercicio1.solution.js
+++ Sergio/ejercicio1.solution.js
@@ -1,5 +1,15 @@
 // Mostrar por pantalla un valor de cada tipo de datos de los que hemos aprendido
 
-console.log([1,3])
-console.log('hola')
+//array
+console.log([1,3]);
+//string
+console.log('hola');
+//operators
+console.log(2 + "5");
+console.log(3 * 6);
+//comparators
+console.log("05" === "5");
+//Boolean
+console.log(true || false);
+//undefinied
 console.log()
