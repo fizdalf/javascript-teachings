cefb6d4f215b7604069b8c81065c5b8f1a87467a
Álvaro <alvaro_am_90@hotmail.com>
2021-02-19 01:28:58 +0000

    Ejercicio1

--- Errowder/Ejercicio 1.js
+++ Errowder/Ejercicio 1.js
@@ -1,1 +1,7 @@
 // Mostrar por pantalla un valor de cada tipo de datos de los que hemos aprendido
+
+console.log(1);  //"tipo numérico";
+console.log("a");  //tipo string
+console.log(Boolean("1"));   //tipo Boolean
+console.log(5/undefined);  //tipo undefined
+console.log([1,2]);  //tipo array
