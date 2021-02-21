52e425ddd30565b6e90e9503465abca967c48d50
Álvaro <alvaro_am_90@hotmail.com>
2021-02-21 01:30:21 +0000

    Ejercicios

--- exercises/controlStructures/if/if.exercise1.js
+++ exercises/controlStructures/if/if.exercise1.js
@@ -1,1 +1,11 @@
 // Ejercicio en el que tome por consola el nombre de un vegetal y si es tomates te responde Junio y si no, te diga que los tomates son geniales.
+
+const arguments = process.argv;
+
+const verdura = arguments [2];
+
+let tomates = "Junio";
+if (verdura !== tomates) {
+    tomates = "Los tomates son geniales";
+}
+console.log(verdura)
