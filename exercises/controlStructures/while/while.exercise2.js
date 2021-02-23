8f6abef238b47cc092ceb7fe0fdd5cd0f7fb2fba
fizdalf <fizdalf@gmail.com>
2021-02-23 08:32:33 +0000

    New while exercises!!

---
+++ exercises/controlStructures/while/while.exercise2.js
@@ -0,0 +1,1 @@
+// Crea un programa que recibe una lista números por terminal, imprime el mayor.
---
+++ exercises/controlStructures/while/while.exercise4.js
@@ -0,0 +1,6 @@
+// Crea un programa que recibe un número por terminal y imprime por pantalla una lista de sus factores, o lo que es lo mismo, los números enteros positivos por los cuales el numero dado es divisible
+// https://www.disfrutalasmatematicas.com/numeros/factores-todos-tool.html
+// Quizás quieras saber que para añadir algo a una lista usamos push
+// const lista = [1,2,3,4];
+// lista.push(7);
+// console.log(lista) -> [1,2,3,4,7]
