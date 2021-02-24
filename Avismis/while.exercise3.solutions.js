40c5c97569702afe69fd9809164c4b431c2e6120
AVISMIS <HELLO@HOTMAIL.COM>
2021-02-24 13:10:44 +0000

    Working on it

--- Avismis/while.exercise3.solutions.js
+++ Avismis/while.exercise3.solutions.js
@@ -1,1 +1,21 @@
 // Crea un programa que recibe una lista de palabras, imprime la más larga, sabiendo que los strings son también listas! ("hola".length -> 4)
+
+const arguments = process.argv;
+let index = 2
+
+const lastIndex = arguments.length - 1;
+
+let longestWord = "a"
+
+while (index <= lastIndex) {
+
+    let word = arguments[index]
+    if (word.length > longestWord.length) {
+        longestWord = word
+    }
+    index++
+}
+
+let message = longestWord
+
+console.log(message)
