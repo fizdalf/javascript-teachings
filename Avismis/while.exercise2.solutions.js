fbd21fdff38ce23438506efc30139a21d088cb6e
AVISMIS <HELLO@HOTMAIL.COM>
2021-02-24 07:45:24 +0000

    Working on it

---
+++ Avismis/while.exercise2.solutions.js
@@ -0,0 +1,15 @@
+// Crea un programa que recibe una lista números por terminal, imprime el mayor.
+
+const arguments = process.argv;
+let index = 2
+
+const lastIndex = arguments.length - 1;
+
+while (index <= lastIndex) {
+   let numbers = arguments;
+    }
+let array.sort(arguments);
+index++;
+console.log(message);
+
+
