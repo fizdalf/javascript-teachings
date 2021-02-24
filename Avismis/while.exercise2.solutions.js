cd9eea0d6c836d0c6c2d1905eb4c0fb4861d0306
AVISMIS <HELLO@HOTMAIL.COM>
2021-02-24 12:26:53 +0000

    Working on it

--- Avismis/while.exercise2.solutions.js
+++ Avismis/while.exercise2.solutions.js
@@ -4,19 +4,22 @@ const arguments = process.argv;
 let index = 2
 
 const lastIndex = arguments.length - 1;
-let max = arguments
+let max = 0
 
 while (index <= lastIndex) {
     let number = arguments[index]
 
+    console.log("el número en la posición " + (index - 2) + " es " + number)
+
     if (number > max) {
         max = number
+        console.log("me quedo con el " + max)
     }
 
     index++;
 }
 
-let message = max
+let message = "de entre "  + (arguments.length - 2) + " números el mayor es " + max
 console.log(message);
 
 
