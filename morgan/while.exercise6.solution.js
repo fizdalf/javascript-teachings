6511892a635b4d8f42a9f788a8a71ce913a2d8bc
Morgan <morgan18stewart@gmail.com>
2021-02-26 19:26:46 +0000

    No están completos, no sabía muy bien como hacerlo, me lie un poco.

---
+++ morgan/while.exercise6.solution.js
@@ -0,0 +1,24 @@
+//8 2 6 9 10 5
+
+
+const arguments = process.argv
+const lastIndex = arguments.length - 1
+
+while (index <= lastIndex) {
+    let index = 2;
+    const listaDeNotas = parseInt(arguments[index])
+    let media = []
+    listaDeNotas.push = media
+    index++
+}
+const lastMedia = media.length - 1
+
+while (indexMedia <= lastMedia) {
+    let suma = 0
+    let indexMedia = 0;
+    (indexMedia + suma = total)
+
+    suma = total
+    indexMedia++
+}
+console.log("la media es " + suma / 2)
---
+++ morgan/while.exercise7.solution.js
@@ -0,0 +1,33 @@
+
+const arguments = process.argv ;
+
+const index = 2;
+const number1 = parseInt(arguments[index]);
+let number2 = 10;
+let tree = "*";
+let firstlevel = 1;
+let levels = firstlevel + 1 + tree;
+
+
+while(firstlevel < levels){
+    console.log(levels)
+    levels++
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
