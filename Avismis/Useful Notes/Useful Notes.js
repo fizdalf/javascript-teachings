cabb7115b94e07359aabe0b4c71ff6d3ac121bc7
AVISMIS <HELLO@HOTMAIL.COM>
2021-02-28 17:00:23 +0000

    Working on it

---
+++ Avismis/Useful Notes/Useful Notes.js
@@ -0,0 +1,12 @@
+// Como medir la cantidad de letras en una palabra dada en terminal
+
+const arguments = process.argv;
+
+let index = arguments[2]
+
+console.log(index.length)
+
+// Como buscar la mitad de esa medida
+
+console.log(index.length / 2)
+
--- Avismis/while/while.exercise9.solutions.js
+++ Avismis/while/while.exercise9.solutions.js
@@ -3,5 +3,51 @@
 // console.log("AAAAAAAAAAAAA".toLowerCase());
 
 
+// const arguments = process.argv;
+//
+// let index = arguments[2]
+//
+// let endOfWord = index.length
+//
+// let halfWord = (index.length / 2)
+//
+// let begginingOfWord = 1
+//
+// // console.log(halfWord)
+// // console.log(endOfWord)
+// // console.log(begginingOfWord)
+//
+// while (begginingOfWord < endOfWord) {
+//     if ((halfWord - 1) === (halfWord + 1)) {
+//         console.log(index + " sí es un palíndromo")
+//     } else if ((halfWord - 1) !== (halfWord + 1)) {
+//         console.log(index + " no es un palíndromo")
+//     }
+//     begginingOfWord++
+// }
+//
+
+
+// let begginngLetter = index.length - (index.length +1)
+//
+// let finalLetter = index.length - 1
+//
+// while (begginngLetter <= finalLetter)
+// {
+//     console.log(arguments[index.length])
+//
+//     console.log(arguments[index.length] / 2)
+// }
+//
+// begginngLetter++
+
+
 const arguments = process.argv;
 
+let index = arguments[2];
+
+let index = [];
+
+// index.length = listOftotalOfLetterInAWord.push;
+
+console.log(index);
