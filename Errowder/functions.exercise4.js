dd8fd890cbe81f8c4a5a5a7a6cf2e82145ccee34
Álvaro <alvaro_am_90@hotmail.com>
2021-03-03 21:12:16 +0000

    Ejercicios

--- Errowder/functions.exercise4.js
+++ Errowder/functions.exercise4.js
@@ -17,12 +17,8 @@ const wallHeight = parseInt(arguments[index + 1])
 const wallWidth = parseInt(arguments[index + 2])
 const numberOfWalls = parseInt(arguments[lastIndex])
 
-function metrosCuadrados(paintCoverRate, wallHeight, wallWidth, numberOfWalls) {
-    return (wallHeight * wallWidth) * numberOfWalls
+function paintCanCalculator(paintCoverRate, wallHeight, wallWidth, numberOfWalls) {
+    return (wallHeight * wallWidth) * numberOfWalls / paintCoverRate
 }
 
-if (paintCoverRate < metrosCuadrados(paintCoverRate, wallHeight, wallWidth, numberOfWalls)) {
-    paintCoverRate = metrosCuadrados(paintCoverRate, wallHeight, wallWidth, numberOfWalls) / paintCoverRate
-}
-
-console.log("You will need " + paintCoverRate + " cans of paint")
+console.log("You will need " + paintCanCalculator() + " cans of paint")
