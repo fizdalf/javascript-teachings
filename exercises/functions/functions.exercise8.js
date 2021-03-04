6c9ee3b25e40322a18d6236278e74d2563e7d722
fizdalf <fizdalf@gmail.com>
2021-03-04 19:05:03 +0000

    Functions

---
+++ exercises/functions/functions.exercise8.js
@@ -0,0 +1,16 @@
+// Oh man! the boss is so happy he wants to give you a raise, but he doesn't know how to calculate it.
+// create function calculateRaise with receives 3 parameters, the base salary, time in years working on the company, and the happiness of the boss level, which is a number from 0 (boss not happy) to 1 (boss is ecstatic!!)
+// The function will calculate how much should be your raised based on the arguments as follows:
+// For every 2 years you have been working on the company you will get an extra 10% of your base salary
+// For every 3 years you have been working on the company you will get a bonus of 2% extra of your base salary
+// For every 5 years you have been working on the company you will get a bonus of 1% extra of your base salary
+// If the happiness of the boss is 0, you don't get a raise!, but if the happiness of the boss is 1, you will get DOUBLE!
+// Account for all values in between of the happiness
+// Example:
+// calculateRaise(2000,10,0.5) -> 1160
+// solve your exercise here below this line
+
+
+// solve your exercise above this line
+// Once you've finished please uncomment the following line as we are going to use this function in the another exercise
+// exports.calculateRaise = calculateRaise;
