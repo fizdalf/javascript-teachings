f69979bbf8ce69221c7ee6d42dcedb0b718362cd
AVISMIS <HELLO@HOTMAIL.COM>
2021-03-10 02:13:22 +0000

    Working on it

--- Avismis/functions/functions.exercise11.solutions.js
+++ Avismis/functions/functions.exercise11.solutions.js
@@ -1,5 +1,18 @@
 // Create a function that receives an array of strings and returns an array of the strings reversed (the strings are in the same order, each string is reversed)
 // Example: reverseStringArray(["something", "computer", "remainder", "monitor"]) -> ["gnihtemos", "retupmoc", "redniamer", "rotinom"]
 
-const exerciseLibrary = require("functions.exercise7.solutions(phrase.backwards)");
+
+function reverseStringArray(words){
+    const reverseCharactersLibrary = require("./functions.exercise7.solutions(phrase.backwards)");
+    let index = 0;
+    let lastIndex = words.length - 1;
+    let reversedWords = '';
+    while (index <= lastIndex){
+    const reversedWords = reverseCharactersLibrary.reverseCharacters[index](words);
+        index++;
+    }
+    return reversedWords;
+}
+    console.log(reverseStringArray("hola que tal"))
+
 
