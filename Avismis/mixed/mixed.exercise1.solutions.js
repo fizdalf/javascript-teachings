0db8e27c8c9dc845459c5c960e81c7bc8399be75
AVISMIS <HELLO@HOTMAIL.COM>
2021-03-17 20:08:02 +0000

    Working on it

--- Avismis/mixed/mixed.exercise1.solutions.js
+++ Avismis/mixed/mixed.exercise1.solutions.js
@@ -1,4 +1,4 @@
-// Create a function Pixel(r,g,b,a) that receives 4 numbers from 0-255 that returns an object that models a pixel in your screen,
+// Create a function Pixel(r,g,b,a) that receives 3 numbers from 0-255 that returns an object that models a pixel in your screen,
 // it has the following properties and methods
 // Pixel
 //  - Properties
@@ -10,11 +10,43 @@
 //      + mixPixel(otherPixel)  This is a method that receives another pixel and returns a new pixel where the each color component will be the average
 //      of the two pixel components values
 // Examples
-// const pixel1 = Pixel(20,250,177,255);
-// const pixel2 = Pixel(255,20,250,255)
-// console.log(pixel1.R) //-> 20
+// const pixel1 = Pixel(20,250,177);
+// const pixel2 = Pixel(255,20,250)
+// console.log(pixel1.R) -> 20
 // const pixel3 = pixel1.mixPixel(pixel2);
-// console.log(pixel3.G) //-> 135
+// console.log(pixel3.G) -> 135
+// 'use strict'
+//
+// function Pixel(r,g,b,a){
+//     return pixel = {
+//         r: 20,
+//         g: 250,
+//         b: 177,
+//         a: 150,
+//         mixPixel: function (pixel2){
+//             pixel2 = {
+//                 r2: 177,
+//                 g2: 250,
+//                 b2: 20,
+//                 a2: 150,
+//             }
+//             let newColorR = (r + r2) / 2
+//             let newColorG = (g + g2) / 2
+//             let newColorB = (b + b2) / 2
+//             let newColorA = (a + a2) / 2
+//
+//             pixel3 = {
+//                 r3: newColorR,
+//                 g3: newColorG,
+//                 b3: newColorB,
+//                 a3: newColorA,
+//             }
+//             return pixel3
+//
+//     }
+//
+//     }
+// }
 'use strict';
 
 function Pixel(r, g, b, a) {
@@ -35,6 +67,3 @@ function Pixel(r, g, b, a) {
 }
 
 exports.Pixel = Pixel;
-
-
-
