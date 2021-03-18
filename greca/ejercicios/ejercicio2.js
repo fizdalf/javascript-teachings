83049fed1e7816f730111ff7fd9746a252fefea1
fizdalf <fizdalf@gmail.com>
2021-03-18 19:48:55 +0000

    Ejercicios

---
+++ greca/ejercicios/ejercicio2.js
@@ -0,0 +1,5 @@
+// Crea un programa que concatena miNombre, una cadena con un sólo espacio " " y miApellido en una nueva variable, luego
+// compara la nueva variable con miNombreCompleto, y si son iguales imprime el mensaje "Nombres iguales" en el terminal
+let miNombre = "Xavier";
+let miApellido = "Gonzalez";
+let miNombreCompleto = "Xavier Gonzalez";
