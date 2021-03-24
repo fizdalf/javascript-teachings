55450b95641b5a0f97f91f1a3b050af875052fee
sergi <sergioarosafdez@gmail.com>
2021-03-24 19:48:30 +0000

    A

---
+++ Sergio/arrayFunctional/arrayFunctional.exercise1.js
@@ -0,0 +1,11 @@
+// Create the function countVowels(word) that receives a word and returns the number of vowels of the word, using array functions like
+// forEach, filter and map as required.
+// Example: countVowels("aeiou") -> 5
+// Example: countVowels("murcielago") -> 5
+
+function countVowels(word){
+    const vocals = ("a","e","i","o","u")
+    const words = ["murcielago"].filter(word !== vocals).map((x) => x.length);
+    return words
+}
+console.log(countVowels())
