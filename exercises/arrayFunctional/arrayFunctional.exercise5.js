1cbb4a690272ea3b15aab7b0d1ed0a18185334c3
fizdalf <fizdalf@gmail.com>
2021-03-25 08:25:44 +0000

    Exercises

---
+++ exercises/arrayFunctional/arrayFunctional.exercise5.js
@@ -0,0 +1,4 @@
+// Create a function forEach(array,callbackFn) that receives an array and a callback function, and calls the function
+// with each of the items of the array
+// Example: forEach([1,2,3], console.log)  -> shows 1 then 2 and the 3 in the terminal
+// Example: forEach(["hola", "adios", "tolete"], (x) => console.log(x.split('').reverse().join('')) -> shows "aloh" then "soida" then "etelot" in the terminal
