1bf466cf5057d527cb6d5f02bfeda67f523a990e
AVISMIS <HELLO@HOTMAIL.COM>
2021-03-27 20:05:23 +0000

    Working on it

--- Avismis/arrayFunctional/arrayFunctional.exercise1.solutions.js
+++ Avismis/arrayFunctional/arrayFunctional.exercise1.solutions.js
@@ -3,21 +3,24 @@
 // Example: countVowels("aeiou") -> 5
 // Example: countVowels("murcielago") -> 5
 
-function countVowels(word) {
-    const allVowels = []
-    word.forEach(word.index).filter("a","e","i","o","u")
 
-    return
+function countVowels(word) {
+    const characterArray = word.split('');
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    // const filteredVowelsInWord = characterArray.filter(character => vowels.filter(vowel => vowel === character).length > 0);
+    // return filteredVowelsInWord.length;
+
+    let vowelsCount = 0;
+
+    characterArray.forEach(character => {
+        if (vowels.filter(vowel => vowel === character).length > 0) {
+            vowelsCount++;
+        }
+    })
+    return vowelsCount;
 
 }
 
-countVowels(murcielago)
+console.log(countVowels('mgfdrtynbmxzcdlkj'));
 
 
-
-
-
-
-
-
-// "a","e","i","o","u"
