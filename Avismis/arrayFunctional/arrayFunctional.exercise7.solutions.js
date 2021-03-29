65b492bb5a370e5dec306e4e7be29a9c06286643
AVISMIS <HELLO@HOTMAIL.COM>
2021-03-29 19:58:24 +0100

    Working on it

--- Avismis/arrayFunctional/arrayFunctional.exercise7.solutions.js
+++ Avismis/arrayFunctional/arrayFunctional.exercise7.solutions.js
@@ -3,3 +3,8 @@
 // (the resulting array must have the same amount of elements)
 // Example: console.log(map([1,2,3], (x) => x ** 2 ))  -> shows [1,4,9] in the terminal
 // Example: console.log(map(["hola", "adios", "tolete"], (x) => x.toUpperCase()) -> shows ["HOLA", "ADIOS", "TOLETE"] in the terminal
+
+function map(array, transformation){
+
+    return lastArray
+}
