a73877281cc5e121e6f3a707605bc34e6d343404
sergi <sergioarosafdez@gmail.com>
2021-04-07 20:00:48 +0100

    Intente

---
+++ Sergio/program/program.exercise3.solution.js
@@ -0,0 +1,54 @@
+// Model a CrowdFunding platform, a project is defined by the GOAL, which is an amount of money they need to raise.
+// The project has a description and also an end date, the project will have tiers that offer interesting rewards to
+// funders, like the following
+// Tier 1: 50€ - A T-shirt with my face in it
+// Tier 2: 100€ - A T-shirt with my face in it and a set of stickers with my face
+// Tier 3: 150€ - A T-shirt with my face in it, and a set of stickers with my face and a cup with my face
+// There must be a way of keeping track of all the funders and their funding
+// There must be a way of telling how much is left to get to the goal
+// When a user provides funding they will receive the reward according to the tiers
+
+const CrowdFunding = function (goal, description, dateOfFinish) {
+    const listOfDonors = new Map();
+    function getDonor(nameOfDonor, donatedMoney){
+        const lowerCaseName = nameOfDonor.toLowerCase();
+        if (listOfDonors.has(nameOfDonor)) {
+            listOfDonors.set(lowerCaseName, donatedMoney);
+        }
+        listOfDonors.set(lowerCaseName, donatedMoney);
+    },
+    return{
+        goal,
+        description,
+        dateOfFinish,
+        Tier(tier1,tier2,tier3){
+            return {
+                tier1: 50,
+                tier2: 100,
+                tier3: 150,
+            }
+        },
+
+        howManyToGoal(moneyFounded){
+            return this.goal - moneyFounded
+        },
+        rewardUser(nameOfDonor, tier){
+
+            const message = "You won a T-short with your face"
+            getDonors(tier)
+            if(nameOfDonor === tier1){
+                return message
+            }
+            if(nameOfDonor === tier2){
+                return message + "and a set of stickers with my face"
+            }
+            if(nameOfDonor === tier3){
+                return message +"and a set of stickers with my face, and a cup with my face"
+            }
+
+        }
+    }
+}
+
+const found1 = CrowdFunding(2000, "me duele el culo", "2021-10-12")
+
---
+++ Sergio/program/program.exercise4.solution.js
@@ -0,0 +1,49 @@
+// Model a Burger Restaurant, you have to keep track of the stock of ingredients, at least 2 burger recipes, each burger
+// can have a set of extras the customer can pick from if they want.
+// The burgers, the menus, the extras, everything will have a price associated.
+// Customers can request full menu or only the burger there must be function that allows to place an order,
+// the function returns the ID of the order and the amount that to be paid, when there's not enough ingredients to fill
+// an order, you must return an error.
+// Another function will take the ID of the order and the money, and will return the actual order
+// (the burgers, fries..whatever) and any spare change.
+// there must be a way of telling how much money has the Burger Restaurant made with their sales
+function Ingredient(nameOfIngredient, quantity) {
+    return {
+        nameOfIngredient,
+        quantity,
+    }
+}
+const stockOfIngredients = [
+    Ingredient( "meat", 80),
+    Ingredient( "bread", 40),
+    Ingredient( "lettuce", 50),
+    Ingredient( "tomato", 60),
+    Ingredient( "onion", 30),
+    Ingredient( "tomato", 60),
+    Ingredient( "chicken", 80),
+]
+const Burger = function (ingredients, price, id) {
+    return {
+        ingredients,
+        price,
+        id,
+    }
+}
+const burgerRestaurant = function(stockOfIngredients, burgers, menus, extras,){
+
+    return{
+        stockOfIngredients,
+        burgers,
+        menus,
+        extras,
+        getBurgerByID: function (id) {
+            if (id < 1 || id > id) {
+                throw Error("Invalid ID");
+            }
+            return Burger(
+                burgers,
+                id,
+            );
+        },
+    }
+}
