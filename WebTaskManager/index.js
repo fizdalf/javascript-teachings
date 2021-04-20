d48e858dc5218b5838c846b0d368b86fd26fb180
fizdalf <fizdalf@gmail.com>
2021-04-20 16:11:32 +0100

    Setting up Web version of taskManager

---
+++ WebTaskManager/index.js
@@ -0,0 +1,7 @@
+import {TaskManager} from "../TaskManager/TaskManager";
+
+const taskManager = TaskManager([{
+    description: 'Ir a comer cosas ricas',
+    completed: false,
+    completedDate: '2021-01-01 20:30:00'
+}])
