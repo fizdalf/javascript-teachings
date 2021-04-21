e69f39d78f80348b8e5138fed5c9d46c6c3f21c7
fizdalf <fizdalf@gmail.com>
2021-04-21 22:52:17 +0100

    Setting up Web version of taskManager

--- WebTaskManager/index.js
+++ WebTaskManager/index.js
@@ -1,7 +1,56 @@
 import {TaskManager} from "../TaskManager/TaskManager";
 
-const taskManager = TaskManager([{
-    description: 'Ir a comer cosas ricas',
-    completed: false,
-    completedDate: '2021-01-01 20:30:00'
-}])
+const taskManager = TaskManager()
+
+
+function renderTasks() {
+    const listContainer = document.getElementById('listContainer')
+    listContainer.innerHTML = "";
+    taskManager.tasks().map((task, index) => {
+        const liElement = document.createElement("li");
+        const descriptionElement = document.createElement('div');
+        descriptionElement.style.display = "inline-block"
+        descriptionElement.innerText = task.description
+        liElement.append(descriptionElement)
+        const completeElement = document.createElement("div");
+        completeElement.style.display = "inline-block"
+        completeElement.innerText = task.completed ? '✔' : "✘"
+        completeElement.addEventListener('click', () => {
+            if (task.completed) {
+                taskManager.markTaskUncompleted(index)
+            } else {
+                taskManager.markAsCompleted(index)
+            }
+            renderTasks();
+        })
+        liElement.append(completeElement)
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "Eliminar"
+        deleteButton.addEventListener('click', () => {
+            taskManager.deleteTask(index);
+            renderTasks();
+        })
+        liElement.append(deleteButton)
+        return liElement;
+    }).forEach(liElement => {
+        listContainer.append(liElement)
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderTasks();
+    const thisIsTheDescriptionInput = document.getElementById('descriptionInput');
+    const buttonToAddTask = document.getElementById('buttonToAddTask');
+    thisIsTheDescriptionInput.addEventListener('change', () => {
+        buttonToAddTask.disabled = !thisIsTheDescriptionInput.value;
+    });
+
+    buttonToAddTask.addEventListener(
+        "click",
+        () => {
+            taskManager.addNewTask(thisIsTheDescriptionInput.value)
+            thisIsTheDescriptionInput.value = ""
+            renderTasks()
+        }
+    );
+});
