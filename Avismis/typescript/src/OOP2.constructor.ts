2eb89a549947948372dbe1562b1c89d8888eaeb6
AVISMIS <HELLO@HOTMAIL.COM>
2021-05-03 20:07:22 +0100

    Working on it

---
+++ Avismis/typescript/src/OOP2.constructor.ts
@@ -0,0 +1,30 @@
+class Robot {
+    private materials = ["metal", "iron", "plastic", "sugar", "spices", "lots of pretty things"];
+    private shape = "humanoide";
+    private creator = "Avismis S.L.";
+    private madeIn = "Ye";
+    private Robot: undefined;
+
+    constructor(){
+
+
+    }
+}
+
+
+const cleaningRobot = new Robot()
+        const cleaningFunction = () => {
+            return "Floors, dishes, windows and every now and then something else"
+        }
+
+
+
+
+    // name = "Pene-López"
+    // protected function = "Talks weird and robotic"
+    // outfit = "Wizard robes"
+
+
+class dancingRobot
+
+}
