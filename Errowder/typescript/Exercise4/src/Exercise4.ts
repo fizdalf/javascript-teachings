cd3166c3e5222d960d6c1550af3a32c9bd623341
Álvaro <alvaro_am_90@hotmail.com>
2021-05-17 02:26:25 +0100

    Ejercicios

---
+++ Errowder/typescript/Exercise4/src/Exercise4.ts
@@ -0,0 +1,29 @@
+//Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (like the name of this kata).
+
+// Strings passed in will consist of only letters and spaces.
+// Spaces will be included only when more than one word is present.
+
+export function reverseWordsWith5OrMoreCharacters(sentence: string) {
+    let test = sentence.split(" ")
+    if (test.length === 1) {
+        if(sentence.length < 5){
+            return sentence
+        }
+        if(sentence.length > 4){
+            return sentence.split("").reverse().join("")
+        }
+    }
+    if(test.length > 1){
+        let finalSentence: string [] = []
+        test.forEach((x)=>{
+            if(x.length < 5){
+                finalSentence.push(x)
+            }
+            if(x.length > 4){
+                x.split("").reverse().join("")
+                finalSentence.push(x)
+            }
+        })
+        return finalSentence.join(" ")
+    }
+}
