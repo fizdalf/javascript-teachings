f1cfc5c8af597cef2f2c09f31072f931247d486c
sergi <sergioarosafdez@gmail.com>
2021-06-11 20:10:52 +0100

    :v

---
+++ Sergio/BuscaMinas.ts
@@ -0,0 +1,24 @@
+const square = document.createElement('div')
+const mina = square.textContent = "MINA" // imagen de una bomba
+const number = square.textContent = "1" // ya cambiaré los posibles números
+
+if (square) {
+    squaresArray.forEach((element) => {
+        const button = document.createElement('button')
+        button.className = "button";
+        button.addEventListener("click", () => {
+            minesWeepers()
+        })
+    })
+}
+
+function minesWeepers() {
+    while (square.textContent !== mina) {
+        if (square.textContent === number) {
+            // el numero debe indicar cuantas minas tiene alrededor
+            return;
+        }
+        //debe mostrar los datos los cuales serán vacios / eliminar el square
+    }
+    alert('You Lose')
+}
